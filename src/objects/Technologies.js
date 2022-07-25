44aaf53d60bbb27bb546ca04f4309a42b08d9cba
EsGui <guilhermedagama2021100@gmail.com>
2022-07-24 21:20:55 -0300

    Nome e imagem das tecnologias aprendidas na trybe foram adicionadas a um array de objetos

---
+++ src/objects/Technologies.js
@@ -0,0 +1,71 @@
+import HTML5 from '../images/technologies/html5.png'
+import CSS from '../images/technologies/css.jpg'
+import JAVASCRIPT from '../images/technologies/js.png'
+import JEST from '../images/technologies/jest.png'
+import REACT from '../images/technologies/react.png'
+import TESTINGLIBRARY from '../images/technologies/testinglibrary.png'
+import DOCKER from '../images/technologies/docker.png'
+import NODEJS from '../images/technologies/nodejs.webp'
+import MOCHA from '../images/technologies/mocha.svg'
+import SINON from '../images/technologies/sinon.png'
+import MYSQL from '../images/technologies/mysql.jpg'
+
+const technologies = [
+  {
+    technologie: 'HTML5',
+    imageTechnologie: HTML5,
+  },
+
+  {
+    technologie: 'CSS',
+    imageTechnologie: CSS,
+  },
+
+  {
+    technologie: 'JAVASCRIPT',
+    imageTechnologie: JAVASCRIPT,
+  },
+
+  {
+    technologie: 'JEST',
+    imageTechnologie: JEST,
+  },
+
+  {
+    technologie: 'REACT',
+    imageTechnologie: REACT,
+  },
+
+  {
+    technologie: 'TESTING LIBRARY',
+    imageTechnologie: TESTINGLIBRARY,
+  },
+
+  {
+    technologie: 'DOCKER',
+    imageTechnologie: DOCKER,
+  },
+
+  {
+    technologie: 'NODEJS',
+    imageTechnologie: NODEJS,
+  },
+
+  {
+    technologie: 'MOCHA',
+    imageTechnologie: MOCHA,
+  },
+
+  {
+    technologie: 'SINON',
+    imageTechnologie: SINON,
+  },
+
+  {
+    technologie: 'MYSQL',
+    imageTechnologie: MYSQL,
+  },
+];
+
+export default technologies;
+
